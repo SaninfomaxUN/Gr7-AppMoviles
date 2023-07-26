892f19856443f669cda36e1bd8ab3e2fe0a4d110
S4N <dcruzhe@unal.edu.co>
2023-07-26 00:06:13 -0500

    Login View

---
+++ app/src/main/java/com/example/ecoapp/Login.java
@@ -0,0 +1,15 @@
+package com.example.ecoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+
+    }
+}
